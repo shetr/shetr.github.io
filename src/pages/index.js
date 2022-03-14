9570c58939ca25ea0e7a8a93c10393cd3b57f2af
Petr Šádek <shetr.git@gmail.com>
2022-03-14 18:44:57 +0100

    basic site

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>Shetr's site about his projects</h1>
+      <p>This site will be used to describe some of my personal projects.</p>
+    </main>
+  )
+}
+
+export default IndexPage
